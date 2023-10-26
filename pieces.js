b1ce7bd8e63d836afe5f0f3d258b760406f21f94
Spratch <contact@josephclenet.fr>
2023-10-26 14:56:21 +0200

    Affichage tous les elements

--- pieces.js
+++ pieces.js
@@ -2,33 +2,38 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-// Indication de la piece a afficher
-const article = pieces[2];
+for (let i=0; i<pieces.length; i++) {
+	// Récupération de l'élément du DOM qui accueillera les fiches
+	const sectionFiches = document.querySelector(".fiches");
 
-// Creation des elements
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
+	// Création d'une balise dédiée à une pièce automobile
+	const pieceElement = document.createElement("article");
 
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
+    // Creation des elements
+    const imageElement = document.createElement("img");
+	imageElement.src = pieces[i].image;
 
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = pieces[i].nom;
 
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(Aucune catégorie)";
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${pieces[i].prix} € (${pieces[i].prix < 35 ? "€" : "€€€"})`;
 
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = pieces[i].categorie ?? "(Aucune catégorie)";
 
-const disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = pieces[i].description ?? "Pas de description pour le moment.";
 
-// Ajout des elements au DOM
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(disponibiliteElement);
+    const disponibiliteElement = document.createElement("p");
+    disponibiliteElement.innerText = pieces[i].disponibilite ? "En stock" : "Rupture de stock";
+
+    // Ajout des elements au DOM
+	sectionFiches.appendChild(pieceElement);
+	pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(disponibiliteElement);
+}
